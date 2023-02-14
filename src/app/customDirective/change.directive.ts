82e97c8a36b452c7f9bfcaf2f3bc51fed2231406
Umesh-25 <124718698+Umesh-25@users.noreply.github.com>
2023-02-14 20:55:55 +0530

    1 file modified

--- src/app/customDirective/change.directive.ts
+++ src/app/customDirective/change.directive.ts
@@ -10,20 +10,41 @@ export class ChangeDirective {
   delayTime = 1000;
   inter;
   logDate: any;
+  checkDate: any;
   constructor(private router: Router, private datePipe: DatePipe) {}
-  @HostListener('mouseleave') onClick() {
+  @HostListener('click') onClick() {
     debugger;
     // console.log('click om  form');
     this.setTimeLeft = 10;
     this.delayTime = 1000;
     this.logDate = this.datePipe.transform(new Date(), 'HH:mm:ss');
+    this.checkDate = this.datePipe.transform(
+      new Date(new Date().getTime() + 2 * 5000),
+      'HH:mm:ss'
+    );
+    // this.checkDate = new Date(new Date().getTime() + 5 * 60000)
     console.log('logDate', this.logDate);
-    this.inter = 0;
-    this.inter = setInterval(() => {
-      if (this.setTimeLeft > 0) {
-        this.setTimeLeft--;
+    console.log('checkDate', this.checkDate);
+    // this.inter = 0;
+    debugger;
+    // while (this.datePipe.transform(new Date(), 'HH:mm:ss') > this.checkDate) {
+    //   console.log('log out');
 
-        console.log('set Intervel', this.setTimeLeft);
+    //   this.router.navigate(['./auth/login']);
+    // }
+    clearInterval(this.inter);
+    this.inter = setInterval(() => {
+      //   if(this.datePipe.transform(new Date(), 'HH:mm:ss') > this.checkDate){
+      //   clearInterval(this.inter);
+      // }
+      if (this.datePipe.transform(new Date(), 'HH:mm:ss') < this.checkDate) {
+        // this.setTimeLeft--;
+        console.log(
+          'current date',
+          this.datePipe.transform(new Date(), 'HH:mm:ss')
+        );
+        console.log('checkDate', this.checkDate);
+        // console.log('set Intervel', this.setTimeLeft);
       } else {
         debugger;
         clearInterval(this.inter);
@@ -31,6 +52,18 @@ export class ChangeDirective {
         this.router.navigate(['./auth/login']);
       }
     }, 1000);
+    // this.inter = setInterval(() => {
+    //   if (this.setTimeLeft > 0) {
+    //     this.setTimeLeft--;
+
+    //     console.log('set Intervel', this.setTimeLeft);
+    //   } else {
+    //     debugger;
+    //     clearInterval(this.inter);
+    //     console.log('clear Intervel');
+    //     this.router.navigate(['./auth/login']);
+    //   }
+    // }, 1000);
   }
 
   interFun(): any {
